3054c3e159a9df9c0ebaed5b205a82af4c635215
khoabin-star <ktran18@huskers.unl.edu>
2023-08-16 05:24:58 +0000

    first commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Khoa Tran
+ * Date: 20xx/xx/xx
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World! My name is Khoa\n");
+
+  return 0;
+}
