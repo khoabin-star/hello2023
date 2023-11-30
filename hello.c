bc1df2d563cd6487655abec0d7d7a0d82bd13bd6
khoabin-star <68915143+khoabin-star@users.noreply.github.com>
2023-11-30 13:32:11 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -14,3 +14,22 @@ int main(int argc, char **argv) {
 
   return 0;
 }
+
+double getAirDistance(const Airport* origin, const Airport* destination) {
+    if (origin == NULL || destination == NULL) {
+        return 1;
+    }
+
+    double latitudeA = origin->latitude;
+    double longitudeA = origin->longitude;
+    double latitudeB = destination->latitude;
+    double longitudeB = destination->longitude;
+
+    double radianLatitudeA = (latitudeA / 180.0) * M_PI;
+    double radianLongitudeA = (longitudeA / 180.0) * M_PI;
+    double radianLatitudeB = (latitudeB / 180.0) * M_PI;
+    double radianLongitudeB = (longitudeB / 180.0) * M_PI;
+    double differenceLongitude = radianLongitudeB - radianLongitudeA;
+    double airDistance = acos(sin(radianLatitudeA) * sin(radianLatitudeB) + cos(radianLatitudeA) * cos(radianLatitudeB) * cos(differenceLongitude)) * radiusEarth;
+    return airDistance;
+}
